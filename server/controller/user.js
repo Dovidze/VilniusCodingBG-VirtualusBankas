0539be97a3407330f9e19dfaac2395a6ac2987ef
Dovydas Andrulis <dovydas.andrulis@vvk.lt>
2025-01-16 14:01:48 +0200

    Server - auth setup

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import User from '../model/user.js';
+import { auth } from '../middleware/auth.js';
 
 const router = Router();
 
@@ -8,17 +9,44 @@ router.get('/', async (req, res) => {
     try {
         res.json(await User.find());
     } catch {
-        res.status(500).json('Įvyko serverio klaida');
+        res.status(501).json('Įvyko serverio klaida');
     }
 });
 
 // Vienas vartotojas
-router.get('/:id', async (req, res) => {
-    try {
-        res.json(await User.findById(req.params.id));
-    } catch {
-        res.status(500).json('Įvyko serverio klaida');
-    }
+// router.get('/:id', async (req, res) => {
+//     try {
+//         res.json(await User.findById(req.params.id));
+//     } catch {
+//         res.status(500).json('Įvyko serverio klaida');
+//     }
+// });
+
+router.get('/check-auth', auth, (req, res) => {
+    res.json(req.session.user);
+});
+
+router.get('/logout', auth, (req, res) => {
+    // Sesijos duomenų ištrynimas
+    req.session.destroy();
+
+    res.json("Sėkmingai atsijungėte");
+});
+
+router.post('/login', async (req, res) => {
+    if(!req.body.login || !req.body.password)
+        return res.status(500).json('Negauti prisijungimo duomenys');
+    const data = await User.findOne({ login: req.body.login, password: req.body.password });
+    
+    if(!data) 
+        return res.status(401).json('Neteisingi prisijungimo duomenys');
+    console.log(data)
+    
+    req.session.user = {
+        login: data.login,
+    };
+
+    res.json(req.session.user);
 });
 
 // Vartotojo sukurimas
@@ -27,7 +55,7 @@ router.post('/', async (req, res) => {
         await User.create(req.body);
         res.json('Įrašas sėkmingai išsaugotas');
     } catch {
-        res.status(500).json('Įvyko serverio klaida');
+        res.status(501).json('Įvyko serverio klaida');
     }
 });
 
@@ -37,7 +65,7 @@ router.put('/:id', async (req, res) => {
         await User.findByIdAndUpdate(req.params.id, req.body)
         res.json("Vartotojas sėkmingai atnaujintas");
     } catch {
-        res.status(500).json('Įvyko serverio klaida');
+        res.status(501).json('Įvyko serverio klaida');
     }
 });
 
@@ -47,7 +75,7 @@ router.delete('/:id', async (req, res) => {
         await User.findByIdAndDelete(req.params.id)
         res.json("Vartotojas sėkmingai ištrintas");
     } catch {
-        res.status(500).json('Įvyko serverio klaida');
+        res.status(501).json('Įvyko serverio klaida');
     }
 });
 
--- server/index.js
+++ server/index.js
@@ -4,12 +4,23 @@ import mongoose from 'mongoose';
 import account from './controller/account.js';
 import user from './controller/user.js';
 
+import session from 'express-session';
+
+
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/account', account);
+app.set('trust proxy', 1) 
+    // Sesijos konfiguracija:
+    app.use(session({
+    secret: 'Slapta unikali frazė', // Slapta unikali frazė
+    resave: false, // Leidžiame slapuko perrašymą
+    saveUninitialized: true, // Išduodame sausainėlį nepriskyrus jokių reikšmių
+    cookie: { secure: false } // Secure nurodo ar kreipsimės per HTTPS protokolą
+    }));
 
+app.use('/api/account', account);
 app.use('/api/user', user);
 
 try {
